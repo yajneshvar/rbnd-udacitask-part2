138568c21cb8a2d73e9643c36386d70944556553
yaj <yajneshvar.arumugam@gmail.com>
2016-02-18 09:16:24 -0500

    Refactored code to use Listable module Added format_description  and format_date to listable

--- lib/listable.rb
+++ lib/listable.rb
@@ -1,3 +1,23 @@
 module Listable
   # Listable methods go here
+  def format_description(description)
+  "#{description}".ljust(30)
+  end
+  def format_date(*dates)
+    if (dates.count == 0)
+       return
+    end
+    if (dates.count > 2)
+        return ""
+    elsif (dates.count == 2)
+       date = dates[0].strftime("%D") if dates[0]
+       date << " -- " + dates[1].strftime("%D") if dates[1]
+    else
+      date = dates[0] ? dates[0].strftime("%D") : "No due date"
+    end
+
+    return date
+    
+  end
+
 end
